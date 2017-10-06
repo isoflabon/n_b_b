5f7110acd3a95a679100a939d176ae69d86a38f1
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-10-06 15:38:24 +0900

    finish creating db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,15 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(content: params[:content])
+    if @post.save
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
 end
