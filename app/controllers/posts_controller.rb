aa2ed41046500e0ccfc5d69bfa643cf080630605
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-10-20 14:07:32 +0900

    make routing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,4 +46,7 @@ class PostsController < ApplicationController
 
   end
 
+  def destroy
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/top'
   root "posts#index"
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update] do
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
     resources :replies, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
