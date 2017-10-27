28551b2be65446e939d54dfc48a10dac4f76a49f
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-10-27 16:23:32 +0900

    current_user kuwaeta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     end
+
+    def set_current_user
+      @current_user = User.find_by(id: session[:id])
+    end
 end
