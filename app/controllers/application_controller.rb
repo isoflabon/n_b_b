2e8343024d4aab34e7dda692579e527bb39bc3f5
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-10-27 17:07:51 +0900

    routingからonlyを消した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-    added_attrs = [ :username, :email, :password, :password_confirmation　]
+    added_attrs = [ :username, :email, :password, :password_confirmation ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
