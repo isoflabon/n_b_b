7094b5e5e2f5aec7f3407914356798503d506cdd
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-10-27 14:32:27 +0900

    UserDBの完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027043655) do
+ActiveRecord::Schema.define(version: 20171027053111) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20171027043655) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.integer "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
