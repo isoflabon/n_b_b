3d30887164fc84b342ff8112b95f3f64bf84c6b6
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-10-28 14:35:06 +0900

    セッションでの現ユーザーの取得と確認コード

--- app/controllers/User/sessions_controller.rb
+++ app/controllers/User/sessions_controller.rb
@@ -2,19 +2,21 @@ class User::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    super
+    # ログインするユーザーのidをセッションとして保持
+    session[:user_id] = @user.id
+  end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # protected
 
