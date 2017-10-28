b4d0979e88be312f1df84f3fe8eca9751fc4a63d
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-10-28 14:29:54 +0900

    投稿のモデルにユーザーを結びつけた

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   has_many :replies
   validates :title, {presence: true,length: {maximum: 40 }}
   validates :content, {presence: true}
+  validates :user_id, {presence: true}
+
+  def user
+    return User.find_by(id: self.user_id)
+  end
 end
