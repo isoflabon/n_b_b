34a1067283af1ecad3af99d34177aaa6b97f05bf
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-10-28 16:06:01 +0900

    コメントアウト

--- app/controllers/User/registrations_controller.rb
+++ app/controllers/User/registrations_controller.rb
@@ -2,30 +2,30 @@ class User::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  GET /resource/sign_up
-  def new
-    super
-  end
-
-  POST /resource
-  def create
-     super
-  end
-
-   GET /resource/edit
-   def edit
-     super
-   end
-
-   PUT /resource
-   def update
-     super
-   end
-
-   DELETE /resource
-   def destroy
-     super
-   end
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+  #
+  # POST /resource
+  # def create
+  #    super
+  # end
+  #
+  #  GET /resource/edit
+  #  def edit
+  #    super
+  #  end
+  #
+  #  PUT /resource
+  #  def update
+  #    super
+  #  end
+  #
+  #  DELETE /resource
+  #  def destroy
+  #    super
+  #  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
