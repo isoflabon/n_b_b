582519c1c637c95b350abfc1e563ae0605eb3346
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-11-02 23:04:27 +0900

    認証用のマイグレーションを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028051215) do
+ActiveRecord::Schema.define(version: 20171102085617) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20171028051215) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.integer "gender"
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
