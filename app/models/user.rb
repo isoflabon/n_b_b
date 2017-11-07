b0bf3103a074aa97a0e8970503e3dbe529a5b997
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-11-07 12:30:07 +0900

    細かな修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,22 +6,15 @@ class User < ApplicationRecord
 
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
-    binding.pry
       unless user
         user = User.create(
           uid:      auth.uid,
           provider: auth.provider,
           email:    auth.extra.raw_info.email,
           password: Devise.friendly_token[0, 20],
-          gender: auth.extra.raw_info.gender
+          gender:   auth.extra.raw_info.gender
           )
       end
     user
   end
-
-  #   private
-  #
-  # def self.dummy_email(auth)
-  #   "#{auth.uid}-#{auth.provider}@example.com"
-  # end
 end
