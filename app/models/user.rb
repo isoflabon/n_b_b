4c7d1a064b74ad35d1195c485fd87a3ee91a7b18
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-11-08 11:50:48 +0900

    新規登録のルーティングを除外した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   def self.find_for_oauth(auth)
