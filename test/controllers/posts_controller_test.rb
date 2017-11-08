37d3e605997a00ac25b88c30838c964647e7c9da
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-11-09 01:23:33 +0900

    見よう見まねでpostsとusersのテストデータの追加

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get posts_path
-  #   assert_response :success
-  # end
+  fixtures :posts
+  
+  test "should get index" do
+    get root_path
+    assert_response :success
+  end
 
 end
