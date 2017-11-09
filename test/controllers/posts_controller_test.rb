91c0e3634562c93d0d9eaa32adcc7d6ae2ceb8b0
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-11-09 16:33:37 +0900

    テスト名の日本語化

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,20 +2,19 @@ require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   fixtures :posts, :users
-  # rootへとぶことができる
-  test "should get root_path" do
+
+  test "rootへとぶことができる" do
     get root_path
     assert_response :success
   end
 
-  # 詳細ページを見る
-  test "should get show" do
+  test "詳細ページを見る" do
     @post = Post.find_by(id: 1)
     get "/posts/#{@post.id}"
     assert_response :success
   end
 
-  test "should get new" do
+  test "新規投稿にとばずリダイレクトされる" do
     get new_post_path
     assert_redirected_to("/users/sign_in")
   end
