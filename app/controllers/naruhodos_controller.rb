4bf58b0c7073c881d1d8eb04a48440a6daf59349
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-11-09 19:21:29 +0900

    「なるほど」を取り消すアクションの実装

--- app/controllers/naruhodos_controller.rb
+++ app/controllers/naruhodos_controller.rb
@@ -1,7 +1,13 @@
 class NaruhodosController < ApplicationController
   def create
     @naruhodo = Naruhodo.new(user_id: @current_user.id, reply_id: params[:reply_id])
+    @naruhodo.save
+    redirect_to("/posts/#{paramns[:post_id]}")
   end
-  @naruhodo.save
-  redirect_to("/posts/#{paramns[:post_id]}")
+
+  def destroy
+    @naruhodo = Naruhodo.find_by(user_id: @current_user.id, reply_id: params[:reply_id])
+    @naruhodo.destroy
+    redirect_to("/posts/#{params[:post_id]}")
+  end
 end
