5d21b7f74a016a5c0d45b399ab21d789db77eab9
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-11-09 21:40:09 +0900

    naruhodoコントローラの変更

--- app/controllers/naruhodos_controller.rb
+++ app/controllers/naruhodos_controller.rb
@@ -1,9 +1,6 @@
 class NaruhodosController < ApplicationController
   def create
-    @naruhodo = Naruhodo.new(
-      user_id: current_user.id,
-      reply_id: @reply
-    )
+    @naruhodo = Naruhodo.new(user_id: current_user.id, reply_id: params[:reply_id])
     @naruhodo.save
     redirect_to("/posts/#{params[:post_id]}")
   end
