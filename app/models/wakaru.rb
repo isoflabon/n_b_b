9052de29463e59732c17f1c904deaceda4c717b0
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-11-09 20:10:27 +0900

    Wakaruモデルを作成

---
+++ app/models/wakaru.rb
@@ -0,0 +1,2 @@
+class Wakaru < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109091205) do
+ActiveRecord::Schema.define(version: 20171109105633) do
 
   create_table "naruhodos", force: :cascade do |t|
     t.integer "user_id"
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 20171109091205) do
     t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wakarus", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "reply_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
