efa731bde631e7f967d7f48630aed27b948f553e
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-11-09 16:06:21 +0900

    postコントローラーのテスト

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,11 +1,24 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  fixtures :posts
-  
-  test "should get index" do
+  fixtures :posts, :users
+  # rootへとぶことができる
+  test "should get root_path" do
     get root_path
     assert_response :success
   end
+
+  # 詳細ページを見る
+  test "should get show" do
+    @post = Post.find_by(id: 1)
+    get "/posts/#{@post.id}"
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_post_path
+    assert_redirected_to("/users/sign_in")
+  end
+
 
 end
