d9b3471c563011080c0dc2099bcc52431d4ff09f
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-11-09 18:29:54 +0900

    なるほどを保存するアクションに対応するようにルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :replies, only: [:create]
   end
+
+  resources :naruhodos, only: [:create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
