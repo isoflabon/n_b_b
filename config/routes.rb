b61b2f057c9a5386e98d2267673ba5402c4cf212
e155737 <e155737@ie.u-ryukyu.ac.jp>
2017-11-13 17:24:27 +0900

    templete追加

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
     resources :replies, only: [:create]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'templete' ,to: 'templete#index'
 end
