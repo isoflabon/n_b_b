9526a80e1517663d20e4cb7b0b247520f6292640
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-11-23 16:27:27 +0900

    /topでtopページを表示するルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   devise_scope :user do
   get '/users/auth/:provider' => 'user/omniauth_callbacks#passthru'
   end
-  get 'home/top'
+  get '/top' => 'home#top'
   root "posts#index"
   resources :posts do
     resources :replies, only: [:create] do
