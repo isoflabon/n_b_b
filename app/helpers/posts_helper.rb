98f7d9d1fa2593d45e3aa9aec9b94ae4e350d141
irish15 <e155756@ie.u-ryukyu.ac.jp>
2017-11-24 15:55:16 +0900

    なるほど/わかるボタンの押された回数と，ログインしているユーザがボタンを既に押しているかをメソッド化した

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,21 @@ module PostsHelper
   def messages(reply_id)
     Reply.where(p_id: reply_id)
   end
+
+  def naruhodoCount(reply_id)
+    Naruhodo.where(reply_id: reply_id).count
+  end
+
+  def wakaruCount(reply_id)
+    Wakaru.where(reply_id: reply_id).count
+  end
+
+  def wakaruAlready(current_user, reply_id)
+    Wakaru.where(user_id: current_user, reply_id: reply_id).present?
+  end
+
+  def naruhodoAlready(current_user, reply_id)
+    Naruhodo.where(user_id: current_user, reply_id: reply_id).present?
+  end
+
 end
