7c9721c332023549819f9a8624f57fe6934d4d7a
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-11-30 18:09:47 +0900

    送信側の設定

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from:     "メールテスト運営局",
+          bcc:      "sample+sent@gmail.com",
+          reply_to: "sample+reply@gmail.com"
   layout 'mailer'
 end
