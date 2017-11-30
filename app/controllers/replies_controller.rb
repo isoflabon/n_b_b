20153435e128e68506414a8e2e3defb0d8d68c6c
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-11-30 19:36:52 +0900

    女性がコメントしたら男性に、男性が返信したら女性に通知が行くようにした

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -11,8 +11,17 @@ class RepliesController < ApplicationController
                        content: content,
                        user_id: current_user.id,
                        p_id: params[:p_id])
+    # 現在のuserが悩みを投稿した男性だった場合
+    if current_user == @post.user
+      # 親の返信をreply_pに格納
+      reply_p = Reply.find_by(id: @reply.p_id)
+      @notice_user = reply_p.user
+    # 悩みを女性がコメントした場合は男性に通知
+    elsif  @reply.user.gender == "female"
+      @notice_user = @post.user
+    end
     if @reply.save
-      NoticeMailer.send_when_reply(@post.user).deliver
+      NoticeMailer.send_when_reply(@notice_user).deliver
       redirect_to("/posts/#{@post.id}")
     else
       flash[:notice] = "コメントを保存できませんでした"
