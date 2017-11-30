79ca40e8415393ac8826454bd0f369857ade884e
kyan-syoya <e155712@ie.u-ryukyu.ac.jp>
2017-11-30 20:50:01 +0900

    Fromをatosu通知に変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from:     "メールテスト運営局",
+  default from:     "atosu通知",
           bcc:      "sample+sent@gmail.com",
           reply_to: "sample+reply@gmail.com"
   layout 'mailer'
