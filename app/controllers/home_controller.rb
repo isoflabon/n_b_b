756268a96e3887e16dd7838877dfdd8c6b582ac4
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-12-05 19:57:30 +0900

    WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def top
   end
+
+  def mypage
+    if current_user.gender == "male"
+      @posts = Post.where(user_id: current_user.id)
+    else
+      @replies = Reply.where(user_id: current_user.id)
+      @replies.each do |reply|
+        temp = Post.find_by(id: reply.post_id)
+        puts temp
+        @posts << temp
+      end
+    end
+  end
 end
