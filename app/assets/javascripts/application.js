564f9b590f8bb0197a02a43e81ef29d0d92cd2bf
e155737 <e155737@ie.u-ryukyu.ac.jp>
2017-12-20 22:24:28 +0900

    headerが下にスクロールすると消え上にスクロールするとfix状態にするjqueryとcssの導入、サイド閉の時ロゴが見えない問題の解消

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,3 +31,20 @@ $(function(){
         });
     });
 });
+
+$(function() {
+  var $win = $(window),
+      $header = $('header'),
+      headerHeight = $header.outerHeight(),
+      startPos = 0;
+
+  $win.on('load scroll', function() {
+    var value = $(this).scrollTop();
+    if ( value > startPos && value > headerHeight ) {
+      $header.css('top', '-' + headerHeight + 'px');
+    } else {
+      $header.css('top', '0');
+    }
+    startPos = value;
+  });
+});
